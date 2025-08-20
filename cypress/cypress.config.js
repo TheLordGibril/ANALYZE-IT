5656dd7190c3356dff2c9585ea8b70a33f078840
TheLordGibril <gabidule03@gmail.com>
2025-08-20 10:45:48 +0200

    Fixed CI Dynamic URL to test all 3 composes

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: process.env.CYPRESS_BASE_URL,
     setupNodeEvents(on, config) {
       require('@cypress/code-coverage/task')(on, config)
       return config
